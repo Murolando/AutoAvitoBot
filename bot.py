30cd472507dc4a7a456a976673f08e297d92824c
Svesh1-800 <edik.khugaev@mail.ru>
2021-07-27 14:28:06 +0300

    Добавил немного безопасности
    - создал файл гитигнор
    - внес туда все необходимое
    - создал обработчик токена
    - сделал скелет бота
    - для тестировки сделал триггер на слово старт

---
+++ bot.py
@@ -0,0 +1,19 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from app.config import get_tg_token
+
+API_TOKEN = get_tg_token()
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
+
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def test(message: types.Message):
+    await message.reply("ABOBA")
+
+if __name__=="__main__":
+    executor.start_polling(dp, skip_updates=True)
