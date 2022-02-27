8cd188c3d94e3688613f67a43928c6a4236cf962
SarnatskyM <57501535+SarnatskyM@users.noreply.github.com>
2022-02-27 18:52:42 +0300

    Add files via upload
    Обновлены марки, доработана цена, рандомные юзерагенты

--- app/handlers/avito.py
+++ app/handlers/avito.py
@@ -1,10 +1,14 @@
 # aiogram
 from ast import parse
+from tkinter.tix import Tree
 from aiogram import types
 from aiogram.types.message import ParseMode
 from bot import dp
 from bot import db
+from aiogram.utils.markdown import hbold, hlink
 
+# user agent
+from fake_useragent import UserAgent
 
 # python
 import re
@@ -31,16 +35,22 @@ async def avito_list(message: types.Message):
                 marka = db.cursor.execute(f"SELECT `avito_mark_name` FROM `marks` WHERE `name` = ?", (line,)).fetchall()
                 marka = marka[0][0]
 
-            max_price = follow[2][0][0]*1000
+            min_price = follow[2][0][0]
+            if follow[3][0][0] == None:
+                max_price = follow[2][0][0]*1000
+            else:
+                max_price = follow[3][0][0]
             #model = "2114_samara"
             #url = f"https://www.avito.ru/{city}/avtomobili/{marka}/{model}?radius={radius}"
+            ua = UserAgent()
             url = f"https://www.avito.ru/{city}/avtomobili/{marka}?radius={radius}"
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+                'User-Agent': f'{ua.random}',
             }
             response = requests.get(url, headers = headers)
             print(response.status_code)
             soup = BeautifulSoup(response.text,'lxml')
+            check_empty = False
             main_container = soup.find_all('div',class_= re.compile('iva-item-content*'))
 
             for index, content in enumerate(main_container):
@@ -53,10 +63,14 @@ async def avito_list(message: types.Message):
                     price = content.find('span', class_=re.compile('price-price-*')).find('meta', itemprop="price")[
                         'content']
 
-                    if fresh_car in allowed_data and int(price) <= max_price: # проверка на сегодняшний день и на подхождение по цене
+                    if fresh_car in allowed_data and int(price) <= max_price and int(price) >= min_price: # проверка на сегодняшний день и на подхождение по цене
 
+                        check_empty = True
+                        
                         title_info = contaier_of_content.find('a',class_=re.compile('iva-item-title*'))['title'].split(',')
                         town_info = contaier_of_content.find('div',class_=re.compile('geo-root*')).find('span').text
+                        datePost_info = contaier_of_content.find('div',class_=re.compile('iva-item-dateInfo*')).find('div').text
+                        print(datePost_info)
                         link = content.find('a', class_=re.compile('iva-item-sliderLink*'))['href']
 
                         currency = content.find('span', class_=re.compile('price-price-*')).find('meta')['content']
@@ -64,7 +78,15 @@ async def avito_list(message: types.Message):
                         was_created = title_info[1]
                         city_on_sale = town_info
 
-                        await message.answer(f"{car_info} - {was_created}. {city_on_sale}\n{price} {currency}\n<a>https://avito.ru{link}</a>",parse_mode=ParseMode.HTML)
+                        card = f'{hlink(car_info+" - "+was_created,"https://avito.ru"+link)}\n' \
+                            f'{hbold("Город: ", city_on_sale)}\n' \
+                            f'{hbold("Цена: ", price, currency)}'
+                        
+                        await message.answer(card)
+
+            #Проверка на то, что есть ли какие то автомобили по нужной цене
+            if not check_empty:
+                await message.answer("Нету автомобилей с нужной вам ценой")
 
     else:
         await message.answer("У вас нет ни одной подписки")
