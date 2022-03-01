6c31e47ed96af801db38b935dd70ccd4632c4a61
SarnatskyM <57501535+SarnatskyM@users.noreply.github.com>
2022-03-01 14:04:18 +0300

    Update avito.py

--- app/handlers/avito.py
+++ app/handlers/avito.py
@@ -21,18 +21,7 @@ def get_session(url):
     session = requests.Session()
     session.headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language':'ru,en-US;q=0.5',
-        'Accept-Encoding':'gzip, deflate, br',
-        'DNT':'1',
-        'Connection':'keep-alive',
-        'Upgrade-Insecure-Requests':'1',
-        'Pragma':'no-cache',
-        'Cache-Control':'no-cache',
-        'sec-ch-ua-platform':'Windows',
-        'referer':url,
-        'sec-fetch-site': 'none',
-        'x-requested-with':'XMLHttpRequest',
     }
     return cfscrape.create_scraper(sess=session)
 
